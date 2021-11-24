63e5ef274b61bad5bd895df75b062fee5841cde2
User <User@ALEXEY>
2021-11-24 13:13:06 +0400

    Добавьте файлы проекта.

---
+++ Ex7.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создать метод для вычисления объема и площади поверхности куба по длине его ребра.
+            Console.WriteLine("Введите длину ребра куба");
+            double x = Convert.ToInt32(Console.ReadLine());
+            double P, S;
+            GetParam(ref x, out P, out S);
+            Console.WriteLine();
+            Console.WriteLine("Площадь поверхности куба = {0}; Объем куба = {1}", P, S);
+            Console.ReadKey();
+        }
+        static void GetParam(ref double x, out double P, out double S)
+        {
+            P = Math.Pow(x, 2) * 6;
+            S = Math.Pow(x, 3);
+        }
+
+    }
+}
---
+++ Exercise7/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex7._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Два треугольника заданы длинами своих сторон.
+            //Определить, площадь какого из них больше (создать метод для вычисления
+            //площади  треугольника по длинам его сторон).
+            //Для решения задачи можно использовать формулу Герона 
+            //S = Sqrt(p*(p-x)*(p-y)*(p-z))
+            //где x, y, z – стороны треугольника, p – полупериметр.
+
+            int[] array = new int[2];
+            int x = 0;
+            int y = 0;
+            int z = 0;
+            int n = 0;
+            double min = 0;
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Введите длины сторон треугольника х, у, z");
+                x = Convert.ToInt32(Console.ReadLine());
+                y = Convert.ToInt32(Console.ReadLine());
+                z = Convert.ToInt32(Console.ReadLine());
+                double S = area(x, y, z);
+                Console.WriteLine();
+                Console.WriteLine("Площадь треугольника = {0:0.000}", S);
+                {
+                    if (S > min)
+                    {
+                        min = S;
+                        n = i + 1;
+                    }
+                }
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("Максимальную площадь имеет треугольник №{0} с площадью = {1:0.000}", n, min);
+            Console.ReadKey();
+        }
+        static double area(int x, int y, int z)
+        {
+            int p = (x + y + z) / 2;
+            double S = Math.Sqrt(p * (p - x) * (p - y) * (p - z));
+            return S;
+        }
+
+    }
+}
